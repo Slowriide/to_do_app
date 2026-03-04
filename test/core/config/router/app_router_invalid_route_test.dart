55c92d008cae2c04ed7b22a14d4ac38e3374b87c
Slowriide <thiagomgobbi7@gmail.com>
2026-03-04 14:15:52 -0300

    Fix router invalid-route tests with required bloc providers

--- test/core/config/router/app_router_invalid_route_test.dart
+++ test/core/config/router/app_router_invalid_route_test.dart
@@ -1,28 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:to_do_app/core/backup/backup_service_base.dart';
+import 'package:to_do_app/core/config/local_storage/local_storage.dart';
 import 'package:to_do_app/core/config/router/app_router.dart';
 import 'package:to_do_app/domain/repository/folder_repository.dart';
 import 'package:to_do_app/domain/repository/note_repository.dart';
 import 'package:to_do_app/domain/repository/todo_repository.dart';
+import 'package:to_do_app/presentation/cubits/folders/folder_cubit.dart';
+import 'package:to_do_app/presentation/cubits/folders/folder_filter_cubit.dart';
+import 'package:to_do_app/presentation/cubits/notes/note_cubit.dart';
+import 'package:to_do_app/presentation/cubits/notes/note_search_cubit.dart';
+import 'package:to_do_app/presentation/cubits/notes/note_view_mode_cubit.dart';
+import 'package:to_do_app/presentation/cubits/theme/theme_cubit.dart';
+import 'package:to_do_app/presentation/cubits/todos/todo_cubit.dart';
+import 'package:to_do_app/presentation/cubits/todos/todo_search_cubit.dart';
 
 import '../../../fake_repositories.dart';
 
 Widget _buildTestApp() {
+  final noteRepository = FakeNoteRepository();
+  final todoRepository = FakeTodoRepository();
+  final folderRepository = FakeFolderRepository();
   return MultiRepositoryProvider(
     providers: [
-      RepositoryProvider<NoteRepository>(create: (_) => FakeNoteRepository()),
-      RepositoryProvider<TodoRepository>(create: (_) => FakeTodoRepository()),
-      RepositoryProvider<FolderRepository>(create: (_) => FakeFolderRepository()),
+      RepositoryProvider<NoteRepository>.value(value: noteRepository),
+      RepositoryProvider<TodoRepository>.value(value: todoRepository),
+      RepositoryProvider<FolderRepository>.value(value: folderRepository),
       RepositoryProvider<BackupService?>.value(value: null),
     ],
-    child: MaterialApp.router(routerConfig: appRouter),
+    child: MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NoteCubit(noteRepository)),
+        BlocProvider(create: (_) => TodoCubit(todoRepository)),
+        BlocProvider(create: (_) => FolderCubit(folderRepository)),
+        BlocProvider(create: (_) => FolderFilterCubit()),
+        BlocProvider(create: (_) => NoteSearchCubit(const [])),
+        BlocProvider(create: (_) => NoteViewModeCubit()),
+        BlocProvider(create: (_) => TodoSearchCubit(const [])),
+        BlocProvider(create: (_) => ThemeCubit()),
+      ],
+      child: MaterialApp.router(routerConfig: appRouter),
+    ),
   );
 }
 
 void main() {
-  setUp(() {
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    await LocalStorage.configurePrefs();
     appRouter.go('/home');
   });
 
